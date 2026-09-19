e7498257191a3277e4ffb85843c90ac6fa9eaa59
000MDK <GitHub.000mdk@t-online.de>
2026-09-19 21:01:43 +0200

    reintegrate PR419 AMD AMF pre-RDNA fallback

--- src/video.cpp
+++ src/video.cpp
@@ -2507,6 +2507,7 @@ namespace video {
     {
       // Common options
       {
+        {"low_power"s, 1},
         {"async_depth"s, 1},
         {"idr_interval"s, std::numeric_limits<int>::max()},
       },
@@ -2514,12 +2515,16 @@ namespace video {
       {},  // HDR-specific options
       {},  // YUV444 SDR-specific options
       {},  // YUV444 HDR-specific options
-      {},  // Fallback options
+      {
+        // Fallback options
+        {"low_power"s, 0},  // Not all VAAPI drivers expose LP entrypoints
+      },
       "av1_vaapi"s,
     },
     {
       // Common options
       {
+        {"low_power"s, 1},
         {"async_depth"s, 1},
         {"sei"s, 0},
         {"idr_interval"s, std::numeric_limits<int>::max()},
@@ -2528,12 +2533,16 @@ namespace video {
       {},  // HDR-specific options
       {},  // YUV444 SDR-specific options
       {},  // YUV444 HDR-specific options
-      {},  // Fallback options
+      {
+        // Fallback options
+        {"low_power"s, 0},  // Not all VAAPI drivers expose LP entrypoints
+      },
       "hevc_vaapi"s,
     },
     {
       // Common options
       {
+        {"low_power"s, 1},
         {"async_depth"s, 1},
         {"sei"s, 0},
         {"idr_interval"s, std::numeric_limits<int>::max()},
@@ -2542,7 +2551,10 @@ namespace video {
       {},  // HDR-specific options
       {},  // YUV444 SDR-specific options
       {},  // YUV444 HDR-specific options
-      {},  // Fallback options
+      {
+        // Fallback options
+        {"low_power"s, 0},  // Not all VAAPI drivers expose LP entrypoints
+      },
       "h264_vaapi"s,
     },
     // RC buffer size will be set in platform code if supported
